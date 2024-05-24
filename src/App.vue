26b7a963d4710f3678ca5f11d677ff4eb9bbf9b7
joao gabriel mota <motajoao41@sempreceub.com>
2024-05-23 23:46:32 -0300

    v 0.1 pagina de login implementada

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <LoginForm/>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import LoginForm from './components/LoginForm.vue'
+
+export default {
+  components: {
+    LoginForm
+  }
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: white;
 }
 </style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="login-wrapper">
+    <form @submit.prevent="submitForm">
+      <h1>Login</h1>
+      <div class="input-box">
+        <input type="text" placeholder="Username" required v-model="username">
+        <i class="bx bxs-user"></i>
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Password" required v-model="password">
+        <i class="bx bxs-lock-alt"></i>
+      </div>
+
+      <div class="remember-forgot">
+        <label><input type="checkbox" v-model="rememberMe">Remember Me</label>
+        <a href="#">Forgot password?</a>
+      </div>
+      
+      <button type="submit" class="btn">Login</button>
+
+      <div class="register-link">
+        <p>Don't have an account?</p><a href="#">Register</a>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      rememberMe: false
+    };
+  },
+  methods: {
+    submitForm() {      console.log('Formulário enviado!');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login-wrapper {
+  width: 420px;
+  background: purple;
+  color: white;
+  border-radius: 10px;
+  padding: 30px 40px;
+  margin: 0 auto; 
+}
+
+.login-wrapper h1 {
+  font-size: 36px;
+  text-align: center;
+  margin-bottom: 20px; 
+}
+
+.login-wrapper .input-box {
+  position: relative; 
+  width: 100%;
+  margin: 30px 0;
+}
+
+.login-wrapper .input-box input {
+  width: 100%;
+  height: 50px;
+  background: transparent;
+  border: none;
+  outline: none;
+  border: 2px solid rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  padding-left: 40px; 
+  color: white; 
+}
+
+.login-wrapper .input-box input::placeholder {
+  color: white;
+}
+
+.login-wrapper .input-box i {
+  position: absolute;
+  left: 10px; 
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 20px;
+  color: white; 
+}
+
+.login-wrapper .remember-forgot {
+  display: flex;
+  justify-content: space-between;
+  font-size: 14.5px;
+  margin: -15px 0 15px;
+}
+
+.login-wrapper .remember-forgot label input {
+  accent-color: white;
+  margin-right: 3px;
+}
+
+.login-wrapper .remember-forgot a {
+  color: white;
+  text-decoration: none;
+}
+
+.login-wrapper .remember-forgot a:hover {
+  text-decoration: underline;
+}
+
+.login-wrapper .btn {
+  width: 100%;
+  height: 45px;
+  background: white;
+  border: none;
+  outline: none;
+  border-radius: 40px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  font-size: 16px;
+  color: #333;
+  font-weight: 600;
+}
+
+.login-wrapper .register-link {
+  font-size: 14.5px;
+  text-align: center;
+  margin-top: 20px;
+}
+
+.login-wrapper .register-link p a {
+  color: white;
+  text-decoration: none;
+  font-weight: 20px 0 15px;
+}
+
+.login-wrapper .register-link p a:hover {
+  text-decoration: underline;
+}
+</style>
