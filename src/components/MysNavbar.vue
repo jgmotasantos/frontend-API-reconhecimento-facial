4e5f580a0bf3721f9e520addacd7174c0be01c17
joao gabriel mota <motajoao41@sempreceub.com>
2024-06-17 20:33:35 -0300

    Bug esquisito

--- src/components/MysNavbar.vue
+++ src/components/MysNavbar.vue
@@ -1,216 +1,214 @@
 <template>
-    <header style="background-color: #1c1c1c; padding-bottom: 10px">
-      <div class="navbar">
-        <div class="logo"><router-link to="/home">Facial API.com</router-link></div>
-        <ul class="links">
-          <li @click="goBack" style="color: white;">Voltar aos detalhes do grupo</li>
-          <li><router-link to="/about">Presenças em Andamento</router-link></li>
-          <li><router-link :to="accessSessionsLink">Minhas Presenças</router-link></li>
-          <li><router-link :to="createSessionLink">Criar Presença</router-link></li>
-        </ul>
-        <router-link to='/auth/login' class="action-btn">Meu Perfil</router-link>
-        <logout-button class="logout-button"></logout-button>
-        <div class="toggle-btn" @click="toggleMenu">
-          <i class="fas fa-bars"></i>
-        </div>
-        <div class="dropdown-menu" :class="{ open: menuOpen }">
-          <ul>
-            <li><router-link to="/">Presença</router-link></li>
-            <li><router-link to="/about">Sobre Nós</router-link></li>
-            <li><router-link to="/services">Serviços</router-link></li>
-            <li><router-link to="/contact">Contato</router-link></li>
-            <li><router-link to="/grupos">Grupos</router-link></li>
-            <li><router-link to="/grupos/criar">Criar Grupo</router-link></li>
-            <li><router-link to="/auth/login" class="action-btn">Meu Perfil</router-link></li>
-          </ul>
-        </div>
+  <header style="background-color: #1c1c1c; padding-bottom: 10px">
+    <div class="navbar">
+      <div class="logo"><router-link to="/home">Facial API.com</router-link></div>
+      <ul class="links">
+        <li @click="goBack" style="color: white;">Voltar aos detalhes do grupo</li>
+        <li><router-link to="/about">Presenças em Andamento</router-link></li>
+        <li><router-link to="/sessoes/iniciar">Minhas Presenças</router-link></li>
+        <li><router-link :to="createSessionLink">Criar Presença</router-link></li>
+      </ul>
+      <router-link to='/auth/login' class="action-btn">Meu Perfil</router-link>
+      <logout-button class="logout-button"></logout-button>
+      <div class="toggle-btn" @click="toggleMenu">
+        <i class="fas fa-bars"></i>
       </div>
-    </header>
-  </template>
-  
-  <script>
-  import LogoutButton from './LogoutButton.vue';
-  import { router, lastRoute } from '@/router'; // Importe o router e a variável lastRoute
-  
-  export default {
-    name: 'MysNavbar',
-    components: {
-      'logout-button': LogoutButton
+      <div class="dropdown-menu" :class="{ open: menuOpen }">
+        <ul>
+          <li><router-link to="/">Presença</router-link></li>
+          <li><router-link to="/about">Sobre Nós</router-link></li>
+          <li><router-link to="/services">Serviços</router-link></li>
+          <li><router-link to="/contact">Contato</router-link></li>
+          <li><router-link to="/grupos">Grupos</router-link></li>
+          <li><router-link to="/grupos/criar">Criar Grupo</router-link></li>
+          <li><router-link to="/auth/login" class="action-btn">Meu Perfil</router-link></li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import LogoutButton from './LogoutButton.vue';
+import { router, lastRoute } from '@/router';
+
+export default {
+  name: 'MysNavbar',
+  components: {
+    'logout-button': LogoutButton
+  },
+  props: {
+    groupName: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      menuOpen: false
+    };
+  },
+  computed: {
+    createSessionLink() {
+      return `/grupos/${this.groupName}/sessoes/iniciar`;
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
     },
-    props: {
-      groupName: {
-        type: String,
-        required: true
-      }
-    },
-    data() {
-      return {
-        menuOpen: false
-      };
-    },
-    computed: {
-      createSessionLink() {
-        return this.groupName ? `/grupos/${this.groupName}/sessoes/iniciar` : '#';
-      }
-    },
-    methods: {
-      toggleMenu() {
-        this.menuOpen = !this.menuOpen;
-      },
-      goBack() {
-        if (lastRoute) {
-          router.push(lastRoute);
-        } else {
-          router.push('/grupos'); // Fallback caso não haja uma última rota registrada
-        }
+    goBack() {
+      if (lastRoute) {
+        router.push(lastRoute);
+      } else {
+        router.push('/grupos'); // Fallback caso não haja uma última rota registrada
       }
     }
-  };
-  </script>
-  
-  <style scoped>
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Poppins', sans-serif;
   }
-  
-  .back-wrapper {
-    margin-bottom: 10px;
-    background-color: #1c1c1c;
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+.back-wrapper {
+  margin-bottom: 10px;
+  background-color: #1c1c1c;
+}
+
+.new-body {
+  background: url('../assets/darkbackground.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: #fff;
+  font-size: 1rem;
+}
+
+a:hover {
+  color: rgb(0, 98, 255);
+}
+
+/* HEADER */
+header {
+  position: relative;
+  padding: 0 2rem;
+}
+
+.navbar {
+  width: 100%;
+  height: 60px;
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.navbar .logo a {
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.navbar .links {
+  display: flex;
+  gap: 2rem;
+}
+
+.navbar .toggle-btn {
+  color: #fff;
+  font-size: 1.5rem;
+  cursor: pointer;
+  display: none;
+}
+
+.action-btn {
+  background-color: rgb(0, 98, 255);
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border: none;
+  outline: none;
+  border-radius: 20px;
+  font-size: 0.8rem;
+  font-weight: bold;
+  cursor: pointer;
+  transition: scale 0.2s ease;
+}
+
+.action-btn:hover {
+  scale: 1.05;
+  color: #fff;
+}
+
+.action-btn:active {
+  scale: 0.95;
+}
+
+.dropdown-menu {
+  display: none;
+  position: absolute;
+  right: 2rem;
+  top: 60px;
+  height: 0;
+  width: 300px;
+  background: rgba(255, 255, 255, 0.2);
+  backdrop-filter: blur(15px);
+  border-radius: 10px;
+  overflow: hidden;
+  transition: height 0.2s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.dropdown-menu li {
+  padding: 0.7rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.dropdown-menu .open {
+  height: 240px;
+}
+
+.dropdown-menu .action-btn {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+/* DESIGN RESPONSIVO */
+@media (max-width: 992px) {
+  .navbar .links,
+  .navbar .action-btn {
+    display: none;
   }
-  
-  .new-body {
-    background: url('../assets/darkbackground.jpg');
-    background-size: cover; 
-    background-position: center; 
-    background-repeat: no-repeat; 
-    position: relative;
-  }
-  
-  li {
-    list-style: none;
-  }
-  
-  a {
-    text-decoration: none;
-    color: #fff;
-    font-size: 1rem;
-  }
-  
-  a:hover {
-    color: rgb(0, 98, 255);
-  }
-  
-  /* HEADER */
-  header {
-    position: relative;
-    padding: 0 2rem;
-  }
-  
-  .navbar {
-    width: 100%;
-    height: 60px;
-    max-width: 1200px;
-    margin: 0 auto;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  
-  .navbar .logo a {
-    font-size: 2rem;
-    font-weight: bold;
-  }
-  
-  .navbar .links {
-    display: flex;
-    gap: 2rem;
-  }
-  
+
   .navbar .toggle-btn {
-    color: #fff;
-    font-size: 1.5rem;
-    cursor: pointer;
-    display: none;
+    display: block;
   }
-  
-  .action-btn {
-    background-color: rgb(0, 98, 255);
-    color: #fff;
-    padding: 0.5rem 1rem;
-    border: none;
-    outline: none;
-    border-radius: 20px;
-    font-size: 0.8rem;
-    font-weight: bold; 
-    cursor: pointer;
-    transition: scale 0.2s ease;  
-  }
-  
-  .action-btn:hover {
-    scale: 1.05;
-    color: #fff;
-  }
-  
-  .action-btn:active {
-    scale: 0.95;
-  }
-  
+
   .dropdown-menu {
-    display: none;
-    position: absolute;
-    right: 2rem;
-    top: 60px;
-    height: 0;
-    width: 300px;
-    background: rgba(255, 255, 255, 0.2);
-    backdrop-filter: blur(15px);
-    border-radius: 10px;
-    overflow: hidden;
-    transition: height 0.2s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    display: block;
   }
-  
-  .dropdown-menu li {
-    padding: 0.7rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+}
+
+@media (max-width: 576px) {
+  .dropdown-menu {
+    left: 2rem;
+    width: unset;
   }
-  
-  .dropdown-menu .open {
-    height: 240px;
-  }
-  
-  .dropdown-menu .action-btn {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-  }
-  
-  /* DESIGN RESPONSIVO */
-  
-  @media (max-width: 992px) {
-    .navbar .links,
-    .navbar .action-btn {
-      display: none;
-    }
-  
-    .navbar .toggle-btn {
-      display: block;
-    }
-  
-    .dropdown-menu {
-      display: block;
-    }
-  }
-  
-  @media (max-width: 576px) {
-    .dropdown-menu {
-      left: 2rem;
-      width: unset;
-    }
-  }
-  </style>
-  
+}
+</style>
